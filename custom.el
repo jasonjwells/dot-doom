d1232dfcf80cb70a6c12afc042a1c4e961ebc32d
Jason Wells <jason@jasonjohnwells.com>
2025-04-22 11:44:36 -0300

    fonts changed

---
+++ custom.el
@@ -0,0 +1,15 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("30d174000ea9cbddecd6cc695943afb7dba66b302a14f9db5dd65074e70cc744"
+     "3061706fa92759264751c64950df09b285e3a2d3a9db771e99bcbb2f9b470037" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
